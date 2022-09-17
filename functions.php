876f1a370fca6c0b79b18a59b1f264b90b244b64
Philip Nachbauer <hello@naba.at>
2022-09-17 10:09:04 +0200

    added mobile menu

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 define('NABA_WP_THEME_NAME', 'hsc-wp-child-theme');
-define('NABA_WP_THEME_VERSION', '2.8.33');
+define('NABA_WP_THEME_VERSION', '2.8.34');
 define('NABA_WP_THEME_PATH', get_stylesheet_directory_uri());
 define('NABA_WP_THEME_SRC', get_stylesheet_directory() . '/src');
 
@@ -15,7 +15,6 @@ add_action( 'wp_enqueue_scripts', 'hsc_styles' );
 
 function hsc_styles() {
     wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css');
-    wp_enqueue_style('naba-hsc-style', get_stylesheet_uri());
     wp_enqueue_script('naba-hsc-scripts', get_stylesheet_directory_uri() . '/assets/dist/scripts.js?ver=' . NABA_WP_THEME_VERSION, [], null, true);
 }
 
